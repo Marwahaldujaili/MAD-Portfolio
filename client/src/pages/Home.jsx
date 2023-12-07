21f809b4e100e48d3288760ca72151a45d2db079
Marwahaldujaili <aldujailimarwa@gmail.com>
2023-12-07 15:21:44 +0100

    home update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import "./styles/Home.scss";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Home() {
   return (
     <div className="home-content">
       <div className="img-container"></div>
       <div className="text-container">
-        <p>Hello! I'm:</p>
+        <p>Hello! I'm</p>
         <ul>
-          <li>Marwah</li>
+          <li>Marwah Al-Dujaili</li>
           <li>a Web Developer</li>
           <li>an IT Trainer/Teacher</li>
           <li>based in Germany</li>
         </ul>
+      </div>
+      <div className="icon-container">
+        <div>
+          <LinkedInIcon />
+        </div>
+        <div>
+          <GitHubIcon />
+        </div>
+        <div>
+          <YouTubeIcon />
+        </div>
       </div>
     </div>
   );
